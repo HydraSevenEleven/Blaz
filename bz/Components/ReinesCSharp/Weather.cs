465e50a398ad14348749ec063f5369283446d4a8
daliacode <daliacode@gmail.com>
2024-10-27 11:06:55 +0000

    InitialCommit

---
+++ bz/Components/ReinesCSharp/Weather.cs
@@ -0,0 +1,16 @@
+namespace bz.Components.ReinesCSharp;
+using bz.Components.ReinesCSharp.DTOs;
+
+public class WeatherLogic
+{
+    public List<WeatherForecast> Logic(){
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = summaries[Random.Shared.Next(summaries.Length)]
+        }).ToList();
+    }
+}
